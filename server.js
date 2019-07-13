e26e96a5532d692205c0e16763740b27e3e9f043
Agu Valeriani <agvaleriani@gmail.com>
2019-07-13 17:01:38 +0200

    -fix deploy.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRE
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 function fetchTweets(req, res) {
